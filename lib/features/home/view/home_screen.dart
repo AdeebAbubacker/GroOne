03bce68bc8cb6ea0a8741e5fba29e589de8694cc
PriteshStetig <pritesh.chitrode@stetig.in>
2025-05-16 14:37:36 +0530

    Create a base structure of stateful view class

--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:gro_one_app/dependency_injection/locator.dart';
+import 'package:gro_one_app/features/sign_in/bloc/sign_in_bloc.dart';
 import 'package:gro_one_app/l10n/extensions/app_localizations_extensions.dart';
 import 'package:gro_one_app/utils/app_application_bar.dart';
+import 'package:gro_one_app/utils/extensions/state_extension.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +13,31 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  final SignInBloc signInBloc = locator<SignInBloc>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    initFunction();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    disposeFunction();
+    super.dispose();
+  }
+
+  void initFunction()=> addPostFrameCallback((){
+    //  Call your init methods
+  });
+
+  void disposeFunction()=> addPostFrameCallback((){
+    signInBloc.close();
+  });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
