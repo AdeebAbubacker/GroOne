21956d2355f9e04b4d33043fb309bd7d1c7f8300
PriteshChitrode <im.priteshchitrode@gmail.com>
2025-05-22 13:01:10 +0530

    Initial Flutter project push to Bitbucket

---
+++ lib/l10n/app_localizations_hi.dart
@@ -0,0 +1,253 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Hindi (`hi`).
+class AppLocalizationsHi extends AppLocalizations {
+  AppLocalizationsHi([String locale = 'hi']) : super(locale);
+
+  @override
+  String get appName => 'ग्रो वन';
+
+  @override
+  String get next => 'अगला';
+
+  @override
+  String get chooseLanguage => 'आप बाद में भी भाषा बदल सकते हैं';
+
+  @override
+  String get choosePreferredLanguage => 'अपना पसंदीदा चुनें';
+
+  @override
+  String get language => 'भाषा';
+
+  @override
+  String get chooseRoleText => 'आप इस ऐप का उपयोग किसलिए करते हैं?';
+
+  @override
+  String get lpTextHeading => 'मैं एक लोड प्रदाता हूँ';
+
+  @override
+  String get lpText => 'मेरे पास परिवहन के लिए वस्तुएँ हैं';
+
+  @override
+  String get vpTextHeading => 'मैं ट्रक प्रदाता हूँ';
+
+  @override
+  String get vpText => 'मैं माल के लिए ट्रक प्रदान कर सकता हूँ';
+
+  @override
+  String get vpLpHeading => 'मैं दोनों करता हूँ';
+
+  @override
+  String get vpLp => 'मेरे पास ट्रक और वस्तुएँ दोनों हैं';
+
+  @override
+  String get fleetHeading => 'मुझे फ्लीट उत्पादों की आवश्यकता है';
+
+  @override
+  String get fleet => 'मैं अपनी फ्लीट के लिए मूल्य वर्धित सेवाएँ चाहता हूँ';
+
+  @override
+  String get getOtp => 'ओटीपी प्राप्त करें';
+
+  @override
+  String get enterMobileNumber => 'जारी रखने के लिए अपना मोबाइल नंबर दर्ज करें।';
+
+  @override
+  String get loginSingUp => 'लॉगिन / साइन अप';
+
+  @override
+  String get agree => 'लॉगिन करके, आप GRO की सहमति देते हैं ';
+
+  @override
+  String get termsAndConditions => 'नियम और शर्तें';
+
+  @override
+  String get and => ' और ';
+
+  @override
+  String get privacyPolicy => 'गोपनीयता नीति';
+
+  @override
+  String get iAgree => 'मैं सहमत हूँ';
+
+  @override
+  String get otpVerification => 'ओटीपी सत्यापन';
+
+  @override
+  String get enterOtpSendNumber => 'भेजे गए कोड को दर्ज करें ';
+
+  @override
+  String get verifyCode => 'कोड सत्यापित करें';
+
+  @override
+  String get resend => 'पुनः भेजें';
+
+  @override
+  String get bookShipment => 'शिपमेंट बुक करें';
+
+  @override
+  String get ourValueAddedServices => 'हमारी वैल्यू ऐडेड सेवाएँ';
+
+  @override
+  String get buyFastTag => 'FASTag खरीदें';
+
+  @override
+  String get enDan => 'एन्-धन कार्ड';
+
+  @override
+  String get gps => 'जीपीएस';
+
+  @override
+  String get instantLoan => 'तत्काल ऋण';
+
+  @override
+  String get kavach => 'KAVACH';
+
+  @override
+  String get insurance => 'बीमा';
+
+  @override
+  String get loginSuccessful => 'लॉगिन सफल';
+
+  @override
+  String get loginSuccessfulSubHeading => 'अब आप दरें देख सकते हैं\nऔर लोड पोस्ट कर सकते हैं';
+
+  @override
+  String get home => 'होम';
+
+  @override
+  String get loads => 'लोड्स';
+
+  @override
+  String get support => 'सहायता';
+
+  @override
+  String get kyc => 'KYC';
+
+  @override
+  String get recentAddedLoad => 'हाल ही में जोड़ा गया लोड';
+
+  @override
+  String get accept => 'स्वीकार करना';
+
+  @override
+  String get seeMore => 'और देखें';
+
+  @override
+  String get availableLoads => 'उपलब्ध भार';
+
+  @override
+  String get filter => 'फ़िल्टर';
+
+  @override
+  String get thisFieldIsRequired => 'यह फ़ील्ड आवश्यक है';
+
+  @override
+  String get vehicleType => 'वाहन प्रकार';
+
+  @override
+  String get selectVehicleType => 'वाहन प्रकार का चयन करें';
+
+  @override
+  String get lane => 'लेन';
+
+  @override
+  String get selectLaneType => 'लेन प्रकार का चयन करें';
+
+  @override
+  String get roadType => 'सड़क प्रकार';
+
+  @override
+  String get selectRoadType => 'सड़क प्रकार का चयन करें';
+
+  @override
+  String get cancel => 'रद्द करना';
+
+  @override
+  String get apply => 'आवेदन करना';
+
+  @override
+  String get tripTracking => 'ट्रिप ट्रैकिंग';
+
+  @override
+  String get allTrip => 'सभी यात्रा';
+
+  @override
+  String get activeTrips => 'सक्रिय यात्राएँ';
+
+  @override
+  String get upcoming => 'आगामी';
+
+  @override
+  String get completed => 'पुरा होना।';
+
+  @override
+  String get createAccount => 'Create Account';
+
+  @override
+  String get name => 'Name';
+
+  @override
+  String get phoneNumber => 'Mobile Number';
+
+  @override
+  String get businessName => 'Business Details';
+
+  @override
+  String get companyName => 'Company Name';
+
+  @override
+  String get truckType => 'Truck Type';
+
+  @override
+  String get ownedTrucks => 'Owned Trucks';
+
+  @override
+  String get attachedTrucks => 'Attached Trucks';
+
+  @override
+  String get preferredLanes => 'Preferred Lanes';
+
+  @override
+  String get businessProof => 'Business Proof';
+
+  @override
+  String get uploadRC => 'Upload RC (for 1 Truck)';
+
+  @override
+  String get uploadDocument => 'Upload document';
+
+  @override
+  String get aadhaarNumber => 'Aadhaar Number(optional)';
+
+  @override
+  String get aadhaarHint => 'xxxx xxxx xxxx';
+
+  @override
+  String get submit => 'Submit';
+
+  @override
+  String get benefitsOfKavach => 'Benefits of KAVACH';
+
+  @override
+  String get benefitsOfKavachHeading1 => 'No Unfair Blame for Fuel Loss';
+
+  @override
+  String get benefitsOfKavachHeading2 => 'Prevents Delay from Theft Investigations';
+
+  @override
+  String get benefitsOfKavachSubHeading1 => 'KAVACH protects drivers from being wrongly accused of fuel theft.';
+
+  @override
+  String get benefitsOfKavachSubHeading2 => 'Keeps drivers on schedule by avoiding time lost to fuel theft issues.';
+
+  @override
+  String get kavachModels => 'KAVACH Models';
+
+  @override
+  String get buyNow => 'Buy Now';
+}
