d324380c4add9694ce25e085c161e0578607b441
PriteshChitrode <im.priteshchitrode@gmail.com>
2025-05-28 21:04:09 +0530

    Load Posing Design Complete

--- lib/features/load_provider/lp_location_screens/lp_select_pick_point/view/lp_select_pick_point_screen.dart
+++ lib/features/load_provider/lp_location_screens/lp_select_pick_point/view/lp_select_pick_point_screen.dart
@@ -1,116 +1,55 @@
-import 'dart:async';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_animations/flutter_map_animations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:geocoding/geocoding.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:go_router/go_router.dart';
-import 'package:gro_one_app/dependency_injection/locator.dart';
-import 'package:gro_one_app/features/load_provider/lp_location_screens/lp_select_pick_point/bloc/lp_map_select_pick_point_bloc.dart';
-import 'package:gro_one_app/l10n/extensions/app_localizations_extensions.dart';
 import 'package:gro_one_app/utils/app_application_bar.dart';
 import 'package:gro_one_app/utils/app_button.dart';
 import 'package:gro_one_app/utils/app_colors.dart';
-import 'package:gro_one_app/utils/app_json.dart';
 import 'package:gro_one_app/utils/app_text_field.dart';
 import 'package:gro_one_app/utils/app_text_style.dart';
-import 'package:gro_one_app/utils/common_widgets.dart';
-import 'package:gro_one_app/utils/constant_variables.dart';
-import 'package:gro_one_app/utils/extensions/int_extensions.dart';
-import 'package:gro_one_app/utils/extensions/state_extension.dart';
-import 'package:gro_one_app/utils/extensions/widget_extensions.dart';
+import 'package:latlong2/latlong.dart';
 
 class LpSelectPickPointScreen extends StatefulWidget {
   const LpSelectPickPointScreen({super.key});
 
   @override
-  State<LpSelectPickPointScreen> createState() => _LpSelectPickPointScreenState();
+  State<LpSelectPickPointScreen> createState() =>
+      _LpSelectPickPointScreenState();
 }
 
-class _LpSelectPickPointScreenState extends State<LpSelectPickPointScreen> {
-
-  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
-  GoogleMapController? mapController;
-
-  final bloc = locator<LpMapSelectPickPointBloc>();
-
-  TextEditingController address = TextEditingController(text: "Coca Cola Bottling Plant, Nemam, Vellavedu, Tamil Nadu 600124");
-
-
-
-  bool locationDone = true;
-  LatLng? centerLatLng;
-  String _address = 'No address found';
-
-  double zoomLevel = 12.0;
+class _LpSelectPickPointScreenState extends State<LpSelectPickPointScreen>
+    with TickerProviderStateMixin {
+  TextEditingController address = TextEditingController(
+    text: "Coca Cola Bottling Plant, Nemam, Vellavedu, Tamil Nadu 600124",
+  );
+  late final AnimatedMapController _animatedMapController;
 
   @override
   void initState() {
-    initFun();
+    _animatedMapController = AnimatedMapController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500), // Animation duration
+      curve: Curves.easeInOut, // Animation curve
+    );
+    _getCurrentLocation();
+    // TODO: implement initState
     super.initState();
   }
 
-  @override
-  void dispose(){
-    disposeFun();
-    super.dispose();
-  }
+  bool locationDone = true;
 
-
-  initFun()=> addPostFrameCallback(() async {
-    bloc.add(FetchCurrentLatLong());
-  });
-
-  disposeFun()=> addPostFrameCallback(() async {
-  });
-
-  Future<void> _setMapStyle(GoogleMapController controller) async {
-    String style = await rootBundle.loadString(AppJSON.mapStyle);
-    controller.setMapStyle(style);
-  }
-
-  //
-  // Future<void> _getCurrentLocation({bool fromAnimate = false}) async {
-  //   bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  //   if (!serviceEnabled) return;
-  //
-  //   LocationPermission permission = await Geolocator.checkPermission();
-  //   if (permission == LocationPermission.denied) {
-  //     permission = await Geolocator.requestPermission();
-  //     if (permission == LocationPermission.denied) return;
-  //   }
-  //
-  //   if (permission == LocationPermission.deniedForever) return;
-  //
-  //   Position position = await Geolocator.getCurrentPosition(
-  //     desiredAccuracy: LocationAccuracy.bestForNavigation,
-  //   );
-  //
-  //   LatLng newLatLng = LatLng(position.latitude, position.longitude);
-  //
-  //   setState(() {
-  //     centerLatLng = newLatLng;
-  //   });
-  //
-  //   await getAddressFromLatLng(newLatLng.latitude, newLatLng.longitude);
-  //
-  //   if (fromAnimate && _mapController != null) {
-  //     _mapController!.animateCamera(
-  //       CameraUpdate.newLatLngZoom(newLatLng, 15.0),
-  //     );
-  //   }
-  // }
+  LatLng? centerLatLng; // Initial center (Adichunchanagiri area)
+  String _address = 'No address found';
 
   Future<void> getAddressFromLatLng(double lat, double lng) async {
     try {
       List<Placemark> placemarks = await placemarkFromCoordinates(lat, lng);
       Placemark place = placemarks.first;
-      setState(() {
-        _address =
-        '${place.street}, ${place.locality}, ${place.country}';
-      });
+      _address = '${placemarks.first.street}, ${placemarks.first.locality}, ${placemarks.first.country}';
+      print('datata  ${place.locality}, ${place.country}');
     } catch (e) {
       setState(() {
         _address = 'Error: $e';
@@ -118,135 +57,206 @@ class _LpSelectPickPointScreenState extends State<LpSelectPickPointScreen> {
     }
   }
 
+  Future<void> _getCurrentLocation({bool fromAnimate = false}) async {
+    bool serviceEnabled;
+    LocationPermission permission;
 
-  Future<BitmapDescriptor> _getMarkerIcon(String category) async {
-    return BitmapDescriptor.defaultMarker;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) return;
+    }
+
+    if (permission == LocationPermission.deniedForever) return;
+
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.medium,
+    );
+
+    setState(() {
+      debugPrint("Position ${position.heading}");
+      centerLatLng = LatLng(position.latitude, position.longitude);
+      getAddressFromLatLng(position.latitude, position.longitude);
+      if (fromAnimate) {
+        _animateToLocation(LatLng(position.latitude, position.longitude));
+      }
+    });
+  }
+
+  void _animateToLocation(LatLng latLng) {
+    _animatedMapController.animateTo(
+      dest: latLng,
+      zoom: 15.0, // Adjust zoom level as needed
+    );
+  }
+
+  @override
+  void dispose() {
+    _animatedMapController.dispose();
+    super.dispose();
+  }
+
+  void _onPositionChanged(MapPosition position, bool hasGesture) {
+    if (position.center != null &&
+        !position.center!.latitude.isNaN &&
+        !position.center!.longitude.isNaN) {
+
+      setState(() {
+        centerLatLng = position.center!;
+        getAddressFromLatLng(centerLatLng!.latitude, centerLatLng!.longitude);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    debugPrint("centerLatLng ${centerLatLng}");
     return Scaffold(
-      appBar: CommonAppBar(title: locationDone ? "Select Pick Point Location" : "Select Destination",),
-      body: BlocBuilder<LpMapSelectPickPointBloc, LpMapSelectPickPointState>(
-        bloc: bloc,
-        builder: (context, state) {
-          if (state is LatLongLoading) {
-            return CircularProgressIndicator().center();
-          } else if (state is LatLongSuccess) {
-            return buildBodyWidget(lat: state.position.latitude, long: state.position.longitude);
-          } else if (state is LatLongError) {
-            return Text("Error: ${state.errorType}");
-          }
-          return 0.height;
-        },
+      appBar: CommonAppBar(
+        title:
+        locationDone ? "Select Pick Point Location" : "Select Destination",
       ),
-    );
-  }
-
-  // Body
-  Widget buildBodyWidget({required double lat, required double long}){
-    return SafeArea(
-      bottom: false,
-      child: Stack(
+      body: Stack(
         children: [
-          // Map view
-          buildMapWidget(lat: lat, long: long),
-
-          // Address
-          buildAddressWidget(context),
-        ],
-      ),
-    );
-  }
-
-  // Map View
-  Widget buildMapWidget({required double lat, required double long}){
-    return Positioned(
-      top: 0,
-      left: 0,
-      right: 0,
-      height: 500.h,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          // Map
-          GoogleMap(
-            mapType: MapType.normal,
-            initialCameraPosition: CameraPosition(target: LatLng(lat, long), zoom: zoomLevel),
-            zoomControlsEnabled: false,
-            zoomGesturesEnabled: true,
-            mapToolbarEnabled: false,
-            onMapCreated: (GoogleMapController controller) async {
-              mapController = controller;
-              await _setMapStyle(controller);
-              if (!_controller.isCompleted) {
-                _controller.complete(controller);
-              }
-            },
-            onTap: (position){
-
-            },
-          ),
-
-          // Marker
-          Icon(CupertinoIcons.location_solid, color: AppColors.primaryColor, size: 30).paddingBottom(70),
-        ],
-      ),
-    );
-  }
-
-
-  Widget buildAddressWidget(BuildContext context){
-    return Positioned(
-      left: 0,
-      right: 0,
-      bottom: 0,
-      child: Container(
-        decoration: commonContainerDecoration(shadow: true),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-
-            Text(context.appText.location, style: AppTextStyle.body),
-            const SizedBox(height: 6),
-            Container(
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child:
+            SizedBox(
+              height: 500.h,
               width: double.infinity,
-              height: 50,
-              decoration: commonContainerDecoration(),
-              child: Text(
-                _address,
-                style: AppTextStyle.textBlackColor14w400.copyWith(
-                  overflow: TextOverflow.ellipsis,
+              child: centerLatLng == null
+                  ? Center(child: CircularProgressIndicator())
+                  :Stack(
+                alignment: Alignment.center,
+                children: [
+                  FlutterMap(
+                    mapController: _animatedMapController.mapController,
+                    options: MapOptions(
+                      // initialCenter: centerLatLng!,
+                      initialCenter: centerLatLng!,
+                      initialZoom: 15.0,
+                      onPositionChanged: _onPositionChanged,
+                    ),
+                    children: [
+                      TileLayer(
+                        urlTemplate:
+                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        //change base_snow_map to pistes
+                        subdomains: ['a', 'b', 'c'],
+                      ),
+
+                      // MarkerLayer to display markers on the map
+                      MarkerLayer(
+                        markers: [
+                          Marker(
+                            // The marker's point is always the current map center.
+                            point: centerLatLng!,
+                            width: 80.0,
+                            height: 80.0,
+                            // Builder function to define the appearance of the marker.
+                            child: const Icon(
+                              Icons.location_pin,
+                              // Using a standard location pin icon
+                              color:
+                              Colors
+                                  .red, // Red color for visibility
+                              size: 40.0, // Size of the icon
+                            ),
+
+                            // Anchor point for the marker (bottom center for a pin)
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    right: 10,
+                    bottom: 30,
+                    child: IconButton(
+                      onPressed: () {
+                        _getCurrentLocation(fromAnimate: true);
+                      },
+                      icon: Icon(
+                        Icons.my_location,
+                        color: AppColors.primaryDarkColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 340.h,
+              decoration: BoxDecoration(
+                color: AppColors.white,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Location", style: AppTextStyle.textBlackColor16w400),
+                    const SizedBox(height: 6),
+                    Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: AppColors.disableColor),
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey.shade200,
+                      ),
+                      child: Text(
+                        _address,
+                        style: AppTextStyle.textBlackColor14w400.copyWith(
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    AppTextField(
+                      controller: address,
+                      labelText: "Address",
+                      labelTextStyle: AppTextStyle.textBlackColor16w400,
+                      maxLines: 3,
+                    ),
+
+                    const SizedBox(height: 20),
+                    AppButton(
+                      title: locationDone ? "Set Pickup Location" : "Continue",
+                      onPressed: () {
+                        if (locationDone == false) {
+                          context.pop();
+                        } else {
+                          locationDone = false;
+                          setState(() {});
+                        }
+                      },
+                    ),
+                  ],
                 ),
               ),
             ),
-            20.height,
-
-
-            AppTextField(
-              controller: address,
-              labelText: context.appText.address,
-              textInputAction: TextInputAction.done,
-            ),
-            30.height,
-
-
-            AppButton(
-              title: locationDone ? "Set Pickup Location" : "Continue",
-              onPressed: () {
-                if (!locationDone) {
-                  context.pop();
-                } else {
-                  locationDone = false;
-                  setState(() {});
-                }
-              },
-            ).bottomButtonPadding(),
-          ],
-        ).paddingAll(commonSafeAreaPadding),
+          ),
+        ],
       ),
     );
   }
-
-
 }
+
