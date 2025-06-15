bfbb06f36c4b7315f6a1c8e8af4bbfa4f3f97c5c
Pritesh Chitrode <pritesh.c@lipitech.in>
2025-06-15 15:31:30 +0530

    Fix Build Issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.gro_one_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -36,6 +36,9 @@ android {
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
+    }
+    dependencies {
+        implementation("com.google.android.gms:play-services-auth:20.7.0")
     }
 }
 
