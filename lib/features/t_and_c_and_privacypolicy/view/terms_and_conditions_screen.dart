0a733a38de0800a85a7e53b56f0fe88d8541785a
Pritesh Chitrode <pritesh.c@lipitech.in>
2025-06-23 15:15:24 +0530

    Fix term & condition bugs fixes

--- lib/features/t_and_c_and_privacypolicy/view/terms_and_conditions_screen.dart
+++ lib/features/t_and_c_and_privacypolicy/view/terms_and_conditions_screen.dart
@@ -17,60 +17,100 @@ class TermsAndConditionsScreen extends StatefulWidget {
 }
 
 class _TermsAndConditionsScreenState extends State<TermsAndConditionsScreen> {
+
   late final WebViewController _controller;
+
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (url) {
+            setState(() {
+              isLoading = true;
+            });
+          },
+          onPageFinished: (url) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+          onWebResourceError: (error) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+        ),
+      )
       ..loadRequest(
         Uri.parse("https://gro-devadmin.letsgro.co/terms-and-condition"),
       );
   }
+
+
   @override
   Widget build(BuildContext context) {
-        return Scaffold(
-      appBar: CommonAppBar(
-        backgroundColor: Colors.transparent,
-        actions: [
-          translateWiget(
-            onTap: () {
-              Navigator.push(
-                context,
-                commonRoute(ChooseLanguageScreen(isCloseButton: true)),
-              );
-            },
-          ),
-          20.width,
-          customerSupportWidget(
-            onTap: () {
-              showCustomerCareBottomSheet(context);
-            },
-          ),
-          20.width,
-          Image.asset(AppImage.png.appIcon, width: 74.25.w, height: 33.h),
-          30.width,
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
-            child: Text(
-              "Terms and Conditions",
-              style: AppTextStyle.textBlackColor30w500,
-            ),
-          ),
-          Expanded(
-            child: WebViewWidget(controller: _controller),
-          ),
-        ],
-      ),
+    return Scaffold(
+      appBar: buildAppBarWidget(context),
+      body: buildBodyWidget(context),
     );
- 
   }
+
+
+  // Body
+  Widget buildBodyWidget(BuildContext context){
+    return Stack(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+              child: Text(
+                "Terms and Conditions",
+                style: AppTextStyle.textBlackColor30w500,
+              ),
+            ),
+            Expanded(child: WebViewWidget(controller: _controller)),
+          ],
+        ),
+        if (isLoading)
+          const Center(
+            child: CircularProgressIndicator(),
+          ),
+      ],
+    );
+  }
+
+
+  PreferredSizeWidget buildAppBarWidget(BuildContext context){
+    return CommonAppBar(
+      backgroundColor: Colors.transparent,
+      actions: [
+        translateWiget(
+          onTap: () {
+            Navigator.push(
+              context,
+              commonRoute(ChooseLanguageScreen(isCloseButton: true)),
+            );
+          },
+        ),
+        20.width,
+        customerSupportWidget(
+          onTap: () {
+            showCustomerCareBottomSheet(context);
+          },
+        ),
+        20.width,
+        Image.asset(AppImage.png.appIcon, width: 74.25.w, height: 33.h),
+        30.width,
+      ],
+    );
+  }
+
 }
