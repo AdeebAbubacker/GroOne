96124925d2fbdf0b33f2493cfec7b8630675dbd1
Pritesh Chitrode <pritesh.c@lipitech.in>
2025-06-24 10:03:32 +0530

    Change Model & build.gradle.kts setting

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode 1
-        versionName "0.0.0" // For UAT
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     buildTypes {
