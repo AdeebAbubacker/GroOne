26008663715aa4b0218008b21a45f4fb83e1f223
Adeeb Abubacker <adeeb.a@doodle-blue.com>
2025-07-07 11:31:12 +0530

    language text done

--- lib/features/choose_language_screen/model/language_model.dart
+++ lib/features/choose_language_screen/model/language_model.dart
@@ -1,56 +1,19 @@
 class LanguageModel {
-    LanguageModel({
-        required this.id,
-        required this.name,
-        required this.languageText,
-        required this.status,
-        required this.createdAt,
-        required this.deletedAt,
-    });
+  final int id;
+  final String name;
+  final String languageText;
 
-    final int id;
-    final String name;
-    final String languageText;
-    final int status;
-    final DateTime? createdAt;
-    final dynamic deletedAt;
+  LanguageModel({
+    required this.id,
+    required this.name,
+    required this.languageText,
+  });
 
-    LanguageModel copyWith({
-        int? id,
-        String? name,
-        String? languageText,
-        int? status,
-        DateTime? createdAt,
-        dynamic? deletedAt,
-    }) {
-        return LanguageModel(
-            id: id ?? this.id,
-            name: name ?? this.name,
-            languageText: languageText ?? this.languageText,
-            status: status ?? this.status,
-            createdAt: createdAt ?? this.createdAt,
-            deletedAt: deletedAt ?? this.deletedAt,
-        );
-    }
-
-    factory LanguageModel.fromJson(Map<String, dynamic> json){ 
-        return LanguageModel(
-            id: json["id"] ?? 0,
-            name: json["name"] ?? "",
-            languageText: json["languageText"] ?? "",
-            status: json["status"] ?? 0,
-            createdAt: DateTime.tryParse(json["createdAt"] ?? ""),
-            deletedAt: json["deletedAt"],
-        );
-    }
-
-    Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-        "languageText": languageText,
-        "status": status,
-        "createdAt": createdAt?.toIso8601String(),
-        "deletedAt": deletedAt,
-    };
-
+  factory LanguageModel.fromJson(Map<String, dynamic> json) {
+    return LanguageModel(
+      id: json['id'],
+      name: json['name'],
+      languageText: json['languageText'],
+    );
+  }
 }
--- lib/features/choose_language_screen/service/language_service.dart
+++ lib/features/choose_language_screen/service/language_service.dart
@@ -3,6 +3,7 @@ import 'package:gro_one_app/data/network/api_service.dart';
 import 'package:gro_one_app/data/network/api_urls.dart';
 import 'package:gro_one_app/features/choose_language_screen/model/language_model.dart';
 
+
 class LanguageService {
   final ApiService _apiService;
   LanguageService(this._apiService);
@@ -10,9 +11,9 @@ class LanguageService {
   Future<Result<List<LanguageModel>>> fetchLanguages() async {
     try {
       final url = ApiUrls.language;
-      final response = await _apiService.get(url, forceRefresh: true);
+      final response = await _apiService.get(url, forceRefresh: true,);
       if (response is Success) {
-        final data = response.value as List;
+        final data = response.value['data'] as List;
         final languages = data.map((e) => LanguageModel.fromJson(e)).toList();
         return Success(languages);
       } else if (response is Error) {
@@ -24,4 +25,5 @@ class LanguageService {
       return Error(DeserializationError());
     }
   }
+
 }
