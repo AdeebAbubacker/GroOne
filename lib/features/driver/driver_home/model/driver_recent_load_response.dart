3281741d6c4165ff294c42927c2d4475473c476f
Adeeb Abubacker <adeeb.a@doodle-blue.com>
2025-07-10 19:18:07 +0530

    Driver Home Updated

---
+++ lib/features/driver/driver_home/model/driver_recent_load_response.dart
@@ -0,0 +1,357 @@
+class DriverRecentLoadResponse {
+  DriverRecentLoadResponse({
+    required this.success,
+    required this.message,
+    required this.data,
+  });
+
+  final bool success;
+  final String message;
+  final List<DriverRecentLoadData> data;
+
+  DriverRecentLoadResponse copyWith({
+    bool? success,
+    String? message,
+    List<DriverRecentLoadData>? data,
+  }) {
+    return DriverRecentLoadResponse(
+      success: success ?? this.success,
+      message: message ?? this.message,
+      data: data ?? this.data,
+    );
+  }
+
+  factory DriverRecentLoadResponse.fromJson(Map<String, dynamic> json){
+    return DriverRecentLoadResponse(
+      success: json["success"] ?? false,
+      message: json["message"] ?? "",
+      data: json["data"] == null ? [] : List<DriverRecentLoadData>.from(json["data"]!.map((x) => DriverRecentLoadData.fromJson(x))),
+    );
+  }
+
+}
+
+class DriverRecentLoadData {
+  DriverRecentLoadData({
+    required this.id,
+    required this.loadId,
+    required this.laneId,
+    required this.rateId,
+    required this.customerId,
+    required this.commodityId,
+    required this.truckTypeId,
+    required this.pickUpAddr,
+    required this.pickUpLocation,
+    required this.assignStatus,
+    required this.pickUpLatlon,
+    required this.dropAddr,
+    required this.dropLocation,
+    required this.dropLatlon,
+    required this.dueDate,
+    required this.consignmentWeight,
+    required this.notes,
+    required this.rate,
+    required this.status,
+    required this.loadStatus,
+    required this.vehicleLength,
+    required this.pickUpDateTime,
+    required this.expectedDeliveryDateTime,
+    required this.handlingCharges,
+    required this.acceptedBy,
+    required this.agreedPrice,
+    required this.acceptedVehicleId,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.deletedAt,
+    required this.commodity,
+    required this.truckType,
+    required this.customer,
+    required this.customerDetail,
+  });
+
+  final int id;
+  final String loadId;
+  final num laneId;
+  final num rateId;
+  final num customerId;
+  final num commodityId;
+  final num truckTypeId;
+  final String pickUpAddr;
+  final String pickUpLocation;
+  final num assignStatus;
+  final String pickUpLatlon;
+  final String dropAddr;
+  final String dropLocation;
+  final String dropLatlon;
+  final DateTime? dueDate;
+  final num consignmentWeight;
+  final String notes;
+  final String rate;
+  final num status;
+  final num loadStatus;
+  final String vehicleLength;
+  final DateTime? pickUpDateTime;
+  final DateTime? expectedDeliveryDateTime;
+  final num handlingCharges;
+  final dynamic acceptedBy;
+  final num agreedPrice;
+  final num acceptedVehicleId;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+  final DateTime? deletedAt;
+  final Commodity? commodity;
+  final TruckType? truckType;
+  final Customer? customer;
+  final CustomerDetail? customerDetail;
+
+  DriverRecentLoadData copyWith({
+    int? id,
+    String? loadId,
+    num? laneId,
+    num? rateId,
+    num? customerId,
+    num? commodityId,
+    num? truckTypeId,
+    String? pickUpAddr,
+    String? pickUpLocation,
+    num? assignStatus,
+    String? pickUpLatlon,
+    String? dropAddr,
+    String? dropLocation,
+    String? dropLatlon,
+    DateTime? dueDate,
+    num? consignmentWeight,
+    String? notes,
+    String? rate,
+    num? status,
+    num? loadStatus,
+    String? vehicleLength,
+    DateTime? pickUpDateTime,
+    DateTime? expectedDeliveryDateTime,
+    num? handlingCharges,
+    dynamic? acceptedBy,
+    num? agreedPrice,
+    num? acceptedVehicleId,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    DateTime? deletedAt,
+    Commodity? commodity,
+    TruckType? truckType,
+    Customer? customer,
+    CustomerDetail? customerDetail,
+  }) {
+    return DriverRecentLoadData(
+      id: id ?? this.id,
+      loadId: loadId ?? this.loadId,
+      laneId: laneId ?? this.laneId,
+      rateId: rateId ?? this.rateId,
+      customerId: customerId ?? this.customerId,
+      commodityId: commodityId ?? this.commodityId,
+      truckTypeId: truckTypeId ?? this.truckTypeId,
+      pickUpAddr: pickUpAddr ?? this.pickUpAddr,
+      pickUpLocation: pickUpLocation ?? this.pickUpLocation,
+      assignStatus: assignStatus ?? this.assignStatus,
+      pickUpLatlon: pickUpLatlon ?? this.pickUpLatlon,
+      dropAddr: dropAddr ?? this.dropAddr,
+      dropLocation: dropLocation ?? this.dropLocation,
+      dropLatlon: dropLatlon ?? this.dropLatlon,
+      dueDate: dueDate ?? this.dueDate,
+      consignmentWeight: consignmentWeight ?? this.consignmentWeight,
+      notes: notes ?? this.notes,
+      rate: rate ?? this.rate,
+      status: status ?? this.status,
+      loadStatus: loadStatus ?? this.loadStatus,
+      vehicleLength: vehicleLength ?? this.vehicleLength,
+      pickUpDateTime: pickUpDateTime ?? this.pickUpDateTime,
+      expectedDeliveryDateTime: expectedDeliveryDateTime ?? this.expectedDeliveryDateTime,
+      handlingCharges: handlingCharges ?? this.handlingCharges,
+      acceptedBy: acceptedBy ?? this.acceptedBy,
+      agreedPrice: agreedPrice ?? this.agreedPrice,
+      acceptedVehicleId: acceptedVehicleId ?? this.acceptedVehicleId,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      deletedAt: deletedAt ?? this.deletedAt,
+      commodity: commodity ?? this.commodity,
+      truckType: truckType ?? this.truckType,
+      customer: customer ?? this.customer,
+      customerDetail: customerDetail ?? this.customerDetail,
+    );
+  }
+
+  factory DriverRecentLoadData.fromJson(Map<String, dynamic> json){
+    return DriverRecentLoadData(
+      id: json["id"] ?? 0,
+      loadId: json["loadId"] ?? "",
+      laneId: json["laneId"] ?? 0,
+      rateId: json["rateId"] ?? 0,
+      customerId: json["customerId"] ?? 0,
+      commodityId: json["commodityId"] ?? 0,
+      truckTypeId: json["truckTypeId"] ?? 0,
+      pickUpAddr: json["pickUpAddr"] ?? "",
+      pickUpLocation: json["pickUpLocation"] ?? "",
+      assignStatus: json["assignStatus"] ?? 0,
+      pickUpLatlon: json["pickUpLatlon"] ?? "",
+      dropAddr: json["dropAddr"] ?? "",
+      dropLocation: json["dropLocation"] ?? "",
+      dropLatlon: json["dropLatlon"] ?? "",
+      dueDate: DateTime.tryParse(json["dueDate"] ?? ""),
+      consignmentWeight: json["consignmentWeight"] ?? 0,
+      notes: json["notes"] ?? "",
+      rate: json["rate"] ?? "",
+      status: json["status"] ?? 0,
+      loadStatus: json["loadStatus"] ?? 0,
+      vehicleLength: json["vehicleLength"] ?? "",
+      pickUpDateTime: DateTime.tryParse(json["pickUpDateTime"] ?? ""),
+      expectedDeliveryDateTime: DateTime.tryParse(json["expectedDeliveryDateTime"] ?? ""),
+      handlingCharges: json["handlingCharges"] ?? 0,
+      acceptedBy: json["acceptedBy"],
+      agreedPrice: json["agreedPrice"] ?? 0,
+      acceptedVehicleId: json["acceptedVehicleId"] ?? 0,
+      createdAt: DateTime.tryParse(json["createdAt"] ?? ""),
+      updatedAt: DateTime.tryParse(json["updatedAt"] ?? ""),
+      deletedAt: DateTime.tryParse(json["deletedAt"] ?? ""),
+      commodity: json["commodity"] == null ? null : Commodity.fromJson(json["commodity"]),
+      truckType: json["truckType"] == null ? null : TruckType.fromJson(json["truckType"]),
+      customer: json["customer"] == null ? null : Customer.fromJson(json["customer"]),
+      customerDetail: json["customerDetail"] == null ? null : CustomerDetail.fromJson(json["customerDetail"]),
+    );
+  }
+
+}
+
+class Commodity {
+  Commodity({
+    required this.id,
+    required this.name,
+    required this.iconUrl,
+  });
+
+  final int id;
+  final String name;
+  final dynamic iconUrl;
+
+  Commodity copyWith({
+    int? id,
+    String? name,
+    dynamic? iconUrl,
+  }) {
+    return Commodity(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      iconUrl: iconUrl ?? this.iconUrl,
+    );
+  }
+
+  factory Commodity.fromJson(Map<String, dynamic> json){
+    return Commodity(
+      id: json["id"] ?? 0,
+      name: json["name"] ?? "",
+      iconUrl: json["iconUrl"],
+    );
+  }
+
+}
+
+class Customer {
+  Customer({
+    required this.id,
+    required this.customerName,
+    required this.mobileNumber,
+    required this.emailId,
+  });
+
+  final int id;
+  final String customerName;
+  final String mobileNumber;
+  final String emailId;
+
+  Customer copyWith({
+    int? id,
+    String? customerName,
+    String? mobileNumber,
+    String? emailId,
+  }) {
+    return Customer(
+      id: id ?? this.id,
+      customerName: customerName ?? this.customerName,
+      mobileNumber: mobileNumber ?? this.mobileNumber,
+      emailId: emailId ?? this.emailId,
+    );
+  }
+
+  factory Customer.fromJson(Map<String, dynamic> json){
+    return Customer(
+      id: json["id"] ?? 0,
+      customerName: json["customerName"] ?? "",
+      mobileNumber: json["mobileNumber"] ?? "",
+      emailId: json["emailId"] ?? "",
+    );
+  }
+
+}
+
+class CustomerDetail {
+  CustomerDetail({
+    required this.id,
+    required this.companyName,
+    required this.companyTypeId,
+  });
+
+  final int id;
+  final String companyName;
+  final num companyTypeId;
+
+  CustomerDetail copyWith({
+    int? id,
+    String? companyName,
+    num? companyTypeId,
+  }) {
+    return CustomerDetail(
+      id: id ?? this.id,
+      companyName: companyName ?? this.companyName,
+      companyTypeId: companyTypeId ?? this.companyTypeId,
+    );
+  }
+
+  factory CustomerDetail.fromJson(Map<String, dynamic> json){
+    return CustomerDetail(
+      id: json["id"] ?? 0,
+      companyName: json["companyName"] ?? "",
+      companyTypeId: json["companyTypeId"] ?? 0,
+    );
+  }
+
+}
+
+class TruckType {
+  TruckType({
+    required this.id,
+    required this.type,
+    required this.subType,
+  });
+
+  final int id;
+  final String type;
+  final String subType;
+
+  TruckType copyWith({
+    int? id,
+    String? type,
+    String? subType,
+  }) {
+    return TruckType(
+      id: id ?? this.id,
+      type: type ?? this.type,
+      subType: subType ?? this.subType,
+    );
+  }
+
+  factory TruckType.fromJson(Map<String, dynamic> json){
+    return TruckType(
+      id: json["id"] ?? 0,
+      type: json["type"] ?? "",
+      subType: json["subType"] ?? "",
+    );
+  }
+
+}
---
+++ lib/features/driver/driver_home/view/widgets/driver_load_widget.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gro_one_app/utils/extensions/int_extensions.dart';
+import 'package:gro_one_app/utils/extensions/widget_extensions.dart';
+
+import '../../../../../utils/app_button.dart';
+import '../../../../../utils/app_button_style.dart';
+import '../../../../../utils/app_colors.dart';
+import '../../../../../utils/app_icons.dart';
+import '../../../../../utils/app_image.dart';
+import '../../../../../utils/app_text_style.dart';
+import '../../../../../utils/common_functions.dart';
+import '../../../../../utils/common_widgets.dart';
+import '../../../../../utils/constant_variables.dart';
+
+class DriverLoadWidget extends StatefulWidget {
+  const DriverLoadWidget({
+    super.key,
+    required this.onClickAssignDriver,
+  });
+
+  final void Function()? onClickAssignDriver;
+
+  @override
+  State<DriverLoadWidget> createState() => _DriverLoadWidgetState();
+}
+
+class _DriverLoadWidgetState extends State<DriverLoadWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+      decoration: commonContainerDecoration(
+        borderColor: AppColors.primaryColor,
+        borderWidth: 1,
+        color: AppColors.blackishWhite,
+      ),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                AppImage.png.truckMyLoad,
+                width: 50,
+              ).paddingSymmetric(vertical: 10),
+              10.width,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('GD 34567', style: AppTextStyle.h5),
+                  Text(
+                  formatDateTimeKavach("2024-08-15T14:30:00Z"),
+                  style: AppTextStyle.primaryColor12w400,
+                  ),
+                ],
+              ).expand(),
+              5.width,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Wrap(
+                    children: [
+                      Text(
+                       "Address",
+                        style: AppTextStyle.blackColor15w500,
+                        maxLines: 2,
+                      ),
+                      Icon(
+                        Icons.arrow_right_alt_outlined,
+                        color: AppColors.primaryColor,
+                      ).paddingSymmetric(horizontal: 2),
+                      Text(
+                        "Drop adress",
+                        style: AppTextStyle.blackColor15w500,
+                        maxLines: 2,
+                      ),
+                    ],
+                  ),
+                  Text('Confirmed', style: AppTextStyle.bodyPurpleColor),
+                ],
+              ).expand(),
+            ],
+          ),
+
+          commonDivider(),
+          //  statusButtonWidget(statusBackgroundColor: AppColors.boxGreen, statusTextColor: AppColors.textGreen, statusText: "Advance Paid")
+          Row(
+            children: [
+              detailWidget(
+                text: "Truck Type",
+                iconSvg: AppIcons.svg.deliveryTruckSpeed,
+              ),
+              detailWidget(
+                text: "Sub Type",
+                iconSvg: AppIcons.svg.deliveryTruckSpeed,
+              ),
+            ],
+          ),
+          10.height,
+          Row(
+            children: [
+              detailWidget(
+                text: "name",
+                iconSvg: AppIcons.svg.package,
+              ),
+              detailWidget(
+                text: "10 Ton",
+                iconSvg: AppIcons.svg.weight,
+              ),
+            ],
+          ),
+          15.height,
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: AppColors.primaryLightColor,
+            ),
+            child: Row(
+              children: [
+                Text(
+                  "Accepted Price",
+                  style: AppTextStyle.textBlackColor18w400,
+                  textAlign: TextAlign.center,
+                ).expand(),
+                Text(
+                  "$indianCurrencySymbol 1000",
+                  style: AppTextStyle.h4PrimaryColor,
+                  textAlign: TextAlign.center,
+                ).expand(),
+              ],
+            ),
+          ),
+          10.height,
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  commonSupportDialog(context);
+                },
+                icon: Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: AppColors.primaryColor,
+                      width: 1.5,
+                    ),
+                  ),
+                  child: SvgPicture.asset(
+                    AppIcons.svg.support,
+                    width: 25,
+                    colorFilter: AppColors.svg(AppColors.primaryColor),
+                  ),
+                ),
+              ),
+              10.width,
+              AppButton(
+                buttonHeight: 40,
+                onPressed: widget.onClickAssignDriver ?? () {},
+                title: "Start Trip",
+                style: AppButtonStyle.primary,
+              ).expand(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget detailWidget({required String text, required String iconSvg}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SvgPicture.asset(
+          iconSvg,
+          width: 18,
+          colorFilter: AppColors.svg(AppColors.black),
+        ),
+        10.width,
+        Text(text, style: AppTextStyle.body),
+      ],
+    ).expand();
+  }
+}
