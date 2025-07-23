b7fbd303e8b3383b9fd29f77eef52c21215dd377
Pritesh Chitrode <pritesh.c@lipitech.in>
2025-07-23 17:59:31 +0530

    Logged Kyc Events

--- lib/service/analytics/analytics_event_name.dart
+++ lib/service/analytics/analytics_event_name.dart
@@ -25,6 +25,14 @@ class AnalyticEventName {
   static const String ONBOARD_VP_FORM_SUBMITTED = "onboard_vp_form_submitted";
   // Lp
   static const String ONBOARD_LP_FORM_SUBMITTED = "onboard_lp_form_submitted";
+  // Kyc
+  static const String AADHAAR_VERIFICATION_SUCCESS = "aadhaar_verification_success";
+  static const String AADHAAR_VERIFICATION_FAILED = "aadhaar_verification_failed";
+  static const String KYC_FORM_SUBMITTED = "kyc_form_submitted";
+  static const String KYC_PENDING = "kyc_pending";
+  static const String KYC_IN_PROGRESS = "kyc_in_progress";
+  static const String KYC_COMPLETED = "kyc_completed";
+
 
 
 }
