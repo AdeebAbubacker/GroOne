feb1bbb77836a3e9bb0f8067286ba153bb134906
Adeeb Abubacker <adeeb.a@doodle-blue.com>
2025-07-28 15:37:32 +0530

    doc id updated

--- lib/features/driver/driver_load_details/cubit/driver_load_details_cubit.dart
+++ lib/features/driver/driver_load_details/cubit/driver_load_details_cubit.dart
@@ -407,7 +407,7 @@ Future<void> getDriverLoadsById({required String loadId}) async {
       final currentList = state.tripDocumentList ?? [];
 
       // Check if POD (TypeId = 8) exists
-      final podIndex = currentList.indexWhere((doc) => doc.documentTypeId == 8);
+      final podIndex = currentList.indexWhere((doc) => doc.documentTypeId == 331);
       if (podIndex != -1 && status != null && status > 6) {
         final updatedDoc = currentList[podIndex].copyWith(visible: true);
         currentList[podIndex] = updatedDoc;
@@ -429,7 +429,7 @@ Future<void> getDriverLoadsById({required String loadId}) async {
 
   bool isPODUploaded(List<DocumentEntity> tripDocumentList) {
   // Just check if the POD document (TypeId 8) is uploaded successfully
-  const podTypeId = 8;
+  const podTypeId = 331;
 
   final presentTypeIds = tripDocumentList
       .where((doc) => doc.loadDocument != null && doc.loadDocument?.status == 1)
