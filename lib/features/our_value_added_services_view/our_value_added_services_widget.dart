6483c16337dc701d5134b420d847796e05161d3e
isangeeta28 <sgeetasharma98@gmail.com>
2025-07-30 17:24:38 +0530

    gps view change

--- lib/features/our_value_added_services_view/our_value_added_services_widget.dart
+++ lib/features/our_value_added_services_view/our_value_added_services_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 // import 'package:gro_one_app/features/gps/view/gps_order_screen.dart';
 import 'package:gro_one_app/features/kavach/view/kavach_orders_list_screen.dart';
 import 'package:gro_one_app/l10n/extensions/app_localizations_extensions.dart';
@@ -13,13 +12,14 @@ import 'package:gro_one_app/utils/extensions/int_extensions.dart';
 import 'package:gro_one_app/utils/extensions/widget_extensions.dart';
 
 import '../../dependency_injection/locator.dart';
-import '../../routing/app_route_name.dart';
 import '../../utils/app_route.dart';
 import '../en-dhan_fuel/view/endhan_new_user_and_card_screen.dart';
 import '../fastag/views/fastag_new_user_screen.dart';
 import '../gps_feature/cubit/gps_order_cubit_folder/gps_kyc_check_cubit.dart';
 import '../gps_feature/gps_order_repo/gps_order_api_repository.dart';
 import '../gps_feature/views/gps_home_screen.dart';
+import '../gps_feature/views/gps_order/gps_order_benefits_and_order_list_screen.dart';
+import '../login/repository/user_information_repository.dart';
 
 class OurValueAddedServicesWidget extends StatefulWidget {
   const OurValueAddedServicesWidget({super.key, this.isGridLayout = false});
@@ -58,121 +58,144 @@ class _OurValueAddedServicesWidgetState
   @override
   Widget build(BuildContext context) {
 
-          Column(
-            children: [
-              SizedBox(
-                height: 101,
-                child: ListView(
-                  controller: _scrollController,
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    15.width,
+    final List<Widget> services = [
+      _buildServicesWidget(
+        title: context.appText.gps,
+        imageString: AppImage.png.gps,
+        onClick: () async {
+          // Check KYC status before navigating
+          final userRepository = locator<UserInformationRepository>();
+          final customerId = await userRepository.getUserID();
 
-                    _buildServicesWidget(
-                      title: context.appText.gps,
-                      imageString: AppImage.png.gps,
-                      onClick: () async {
-                        // Check KYC status before navigating
-                        final userRepository = locator<UserInformationRepository>();
-                        final customerId = await userRepository.getUserID();
+          if (customerId != null && customerId.isNotEmpty) {
+            // Create a temporary cubit to check KYC
+            final kycCheckCubit = GpsKycCheckCubit(
+              locator<GpsOrderApiRepository>(),
+            );
 
-                        if (customerId != null && customerId.isNotEmpty) {
-                          // Create a temporary cubit to check KYC
-                          final kycCheckCubit = GpsKycCheckCubit(
-                            locator<GpsOrderApiRepository>(),
-                          );
+            // Show loading dialog
+            showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (BuildContext context) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+            );
 
-                          // Show loading dialog
-                          showDialog(
-                            context: context,
-                            barrierDismissible: false,
-                            builder: (BuildContext context) {
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            },
-                          );
+            // Check KYC documents
+            await kycCheckCubit.checkKycDocuments(customerId);
 
-                          // Check KYC documents
-                          await kycCheckCubit.checkKycDocuments(customerId);
+            // Close loading dialog
+            if (context.mounted) {
+              Navigator.of(context).pop();
+            }
 
-                          // Close loading dialog
-                          if (context.mounted) {
-                            Navigator.of(context).pop();
-                          }
+            // Navigate based on KYC status
+            if (context.mounted) {
+              if (kycCheckCubit.state.hasKycDocuments &&
+                  kycCheckCubit.state.kycData != null) {
+                // KYC done - check order list to decide between GPS home or benefits
+                Navigator.push(
+                  context,
+                  commonRoute(GpsOrderBenefitsAndOrderListScreen()),
+                );
+              } else {
+                // KYC not done - show benefits screen
+                Navigator.push(
+                  context,
+                  commonRoute(
+                    GpsOrderBenefitsAndOrderListScreen(),
+                  ),
+                );
+              }
+            }
 
-                          // Navigate based on KYC status
-                          if (context.mounted) {
-                            if (kycCheckCubit.state.hasKycDocuments &&
-                                kycCheckCubit.state.kycData != null) {
-                              // KYC done - check order list to decide between GPS home or benefits
-                              Navigator.push(
-                                context,
-                                commonRoute(GpsOrderBenefitsAndOrderListScreen()),
-                              );
-                            } else {
-                              // KYC not done - show benefits screen
-                              Navigator.push(
-                                context,
-                                commonRoute(
-                                  GpsOrderBenefitsAndOrderListScreen(),
-                                ),
-                              );
-                            }
-                          }
-
-          //   // Show loading dialog
-          //   showDialog(
-          //     context: context,
-          //     barrierDismissible: false,
-          //     builder: (BuildContext context) {
-          //       return const Center(
-          //         child: CircularProgressIndicator(),
-          //       );
-          //     },
-          //   );
-
-          //   // Check KYC documents
-          //   await kycCheckCubit.checkKycDocuments(customerId);
-
-          //   // Close loading dialog
-          //   if (context.mounted) {
-          //     Navigator.of(context).pop();
-          //   }
-
-          //   // Navigate based on KYC status
-          //   if (context.mounted) {
-          //     if (kycCheckCubit.state.hasKycDocuments &&
-          //         kycCheckCubit.state.kycData != null) {
-          //       // KYC done - show GPS home screen
-          //       Navigator.push(
-          //         context,
-          //         commonRoute(GpsHomeScreen()),
-          //         //commonRoute(GpsOrderBenefitsAndOrderListScreen()),
-
-          //       );
-          //     } else {
-          //       // KYC not done - show benefits screen
-          //       Navigator.push(
-          //         context,
-          //         commonRoute(
-          //           GpsOrderBenefitsAndOrderListScreen(),
-          //         ),
-          //       );
-          //     }
-          //   }
-
-          //   // Dispose the temporary cubit
-          //   kycCheckCubit.close();
-          // } else {
-          //   // Fallback to benefits screen if customer ID not available
-          //   Navigator.push(
-          //     context,
-          //     commonRoute(GpsOrderBenefitsAndOrderListScreen()),
-          //   );
-          // }
+            // Dispose the temporary cubit
+            kycCheckCubit.close();
+          } else {
+            // Fallback to benefits screen if customer ID not available
+            Navigator.push(
+              context,
+              commonRoute(GpsOrderBenefitsAndOrderListScreen()),
+            );
+          }
         },
       ),
+      // _buildServicesWidget(
+      //   title: context.appText.gps,
+      //   imageString: AppImage.png.gps,
+      //   onClick: () async {
+      //     Navigator.push(
+      //       context,
+      //       commonRoute(GpsHomeScreen()),
+      //
+      //       //commonRoute(GpsOrderBenefitsAndOrderListScreen()),
+      //     );
+      //     // Check KYC status before navigating
+      //     // final userRepository =
+      //     //     locator<UserInformationRepository>();
+      //     // final customerId = await userRepository.getUserID();
+      //
+      //     // if (customerId != null && customerId.isNotEmpty) {
+      //     //   // Create a temporary cubit to check KYC
+      //     //   final kycCheckCubit = GpsKycCheckCubit(
+      //     //     locator<GpsOrderApiRepository>(),
+      //     //   );
+      //
+      //     //   // Show loading dialog
+      //     //   showDialog(
+      //     //     context: context,
+      //     //     barrierDismissible: false,
+      //     //     builder: (BuildContext context) {
+      //     //       return const Center(
+      //     //         child: CircularProgressIndicator(),
+      //     //       );
+      //     //     },
+      //     //   );
+      //
+      //     //   // Check KYC documents
+      //     //   await kycCheckCubit.checkKycDocuments(customerId);
+      //
+      //     //   // Close loading dialog
+      //     //   if (context.mounted) {
+      //     //     Navigator.of(context).pop();
+      //     //   }
+      //
+      //     //   // Navigate based on KYC status
+      //     //   if (context.mounted) {
+      //     //     if (kycCheckCubit.state.hasKycDocuments &&
+      //     //         kycCheckCubit.state.kycData != null) {
+      //     //       // KYC done - show GPS home screen
+      //     //       Navigator.push(
+      //     //         context,
+      //     //         commonRoute(GpsHomeScreen()),
+      //     //         //commonRoute(GpsOrderBenefitsAndOrderListScreen()),
+      //
+      //     //       );
+      //     //     } else {
+      //     //       // KYC not done - show benefits screen
+      //     //       Navigator.push(
+      //     //         context,
+      //     //         commonRoute(
+      //     //           GpsOrderBenefitsAndOrderListScreen(),
+      //     //         ),
+      //     //       );
+      //     //     }
+      //     //   }
+      //
+      //     //   // Dispose the temporary cubit
+      //     //   kycCheckCubit.close();
+      //     // } else {
+      //     //   // Fallback to benefits screen if customer ID not available
+      //     //   Navigator.push(
+      //     //     context,
+      //     //     commonRoute(GpsOrderBenefitsAndOrderListScreen()),
+      //     //   );
+      //     // }
+      //   },
+      // ),
       _buildServicesWidget(
         title: "Fuel Card",
         imageString: AppImage.png.enDhan,
@@ -184,12 +207,12 @@ class _OurValueAddedServicesWidgetState
           );
         },
       ),
-                    _buildServicesWidget(
-                      title: "Fast tag",
-                      imageString: AppImage.png.buyFastTag,
-                      onClick: () {
-                       // context.push(AppRouteName.buyFastag);
-                        Navigator.push(context, commonRoute(FastagNewUserScreen()));
+      _buildServicesWidget(
+        title: "Fast tag",
+        imageString: AppImage.png.buyFastTag,
+        onClick: () {
+          // context.push(AppRouteName.buyFastag);
+          Navigator.push(context, commonRoute(FastagNewUserScreen()));
         },
       ),
       _buildServicesWidget(
