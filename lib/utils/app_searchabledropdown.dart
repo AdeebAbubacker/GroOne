db377dafecd7dda8c5e881603f8239244d10b31c
Adeeb Abubacker <adeeb.a@doodle-blue.com>
2025-08-04 16:48:04 +0530

    dropdwon updated

--- lib/utils/app_searchabledropdown.dart
+++ lib/utils/app_searchabledropdown.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:dropdown_search/dropdown_search.dart';
+import 'package:gro_one_app/utils/app_text_style.dart';
 import 'package:gro_one_app/utils/common_widgets.dart';
+import 'package:gro_one_app/utils/extensions/int_extensions.dart';
 
 class SearchableDropdown extends StatelessWidget {
   final String? selectedItem;
@@ -8,6 +10,9 @@ class SearchableDropdown extends StatelessWidget {
   final String hintText;
   final ValueChanged<String?> onChanged;
   final bool showSearchBox;
+  final String? labelText;
+  final bool mandatoryStar;
+  final TextStyle? labelTextStyle;
   final DropdownSearchBuilder<String>? dropdownBuilder;
   final Widget Function(BuildContext, String)? emptyBuilder;
   
@@ -21,33 +26,57 @@ class SearchableDropdown extends StatelessWidget {
     this.showSearchBox = true,
     this.dropdownBuilder,
     this.emptyBuilder,
+     this.labelText,
+    this.mandatoryStar = false,
+    this.labelTextStyle,
   });
 
   @override
   Widget build(BuildContext context) {
-    return DropdownSearch<String>(
-      selectedItem: selectedItem,
-      items: (String filter, _) async {
-        return items
-            .where((v) => v.toLowerCase().contains(filter.toLowerCase()))
-            .toList();
-      },
-      popupProps: PopupProps.menu(
-        showSearchBox: showSearchBox,
-        // constraints: const BoxConstraints(maxHeight: 250),
-        emptyBuilder:  emptyBuilder ?? (context, searchEntry) => const Center(
-          child: Text("No results found"),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (labelText != null)
+          Row(
+            children: [
+              Text(
+                " $labelText",
+                style: labelTextStyle ?? AppTextStyle.textFiled,
+              ),
+              if (mandatoryStar)
+                Text(
+                  " *",
+                  style: (labelTextStyle ?? AppTextStyle.textFiled)
+                      .copyWith(color: Colors.red),
+                ),
+            ],
+          ),
+        if (labelText != null) 6.height,
+        DropdownSearch<String>(
+          selectedItem: selectedItem,
+          items: (String filter, _) async {
+            return items
+                .where((v) => v.toLowerCase().contains(filter.toLowerCase()))
+                .toList();
+          },
+          popupProps: PopupProps.menu(
+            showSearchBox: showSearchBox,
+            // constraints: const BoxConstraints(maxHeight: 250),
+            emptyBuilder:  emptyBuilder ?? (context, searchEntry) => const Center(
+              child: Text("No results found"),
+            ),
+            loadingBuilder: (context, searchEntry) =>
+                const Center(child: CircularProgressIndicator()),
+          ),
+          decoratorProps: DropDownDecoratorProps(
+            decoration: commonInputDecoration(
+              hintText: hintText,
+            ),
+          ),
+          onChanged: onChanged,
+          dropdownBuilder: dropdownBuilder,
         ),
-        loadingBuilder: (context, searchEntry) =>
-            const Center(child: CircularProgressIndicator()),
-      ),
-      decoratorProps: DropDownDecoratorProps(
-        decoration: commonInputDecoration(
-          hintText: hintText,
-        ),
-      ),
-      onChanged: onChanged,
-      dropdownBuilder: dropdownBuilder,
+      ],
     );
   }
 }
