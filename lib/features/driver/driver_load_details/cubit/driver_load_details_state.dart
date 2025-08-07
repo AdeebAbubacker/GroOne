0d652fd351c114f0686ce4d4d4469a9fee917436
Adeeb Abubacker <adeeb.a@doodle-blue.com>
2025-08-08 04:05:10 +0530

    awb formatter added

--- lib/features/driver/driver_load_details/cubit/driver_load_details_state.dart
+++ lib/features/driver/driver_load_details/cubit/driver_load_details_state.dart
@@ -1,6 +1,5 @@
 part of 'driver_load_details_cubit.dart';
 
-
 class DriverLoadDetailsState extends Equatable {
   final LoadStatus? loadStatus;
   final UIState<DriverLoadDetailsModel>? lpLoadById;
@@ -9,7 +8,7 @@ class DriverLoadDetailsState extends Equatable {
   final UIState<VpLoadAcceptModel>? loadStatusUIState;
   final UIState<DeleteDamageModel>? deleteDamageUIState;
   final bool? isUpdateDamage;
-  final List<DocumentEntity>? tripDocumentList ;
+  final List<DocumentEntity>? tripDocumentList;
   final UIState<DamageModel>? createDamageUIState;
   final UIState<DamageModel>? settlementUIState;
   final UIState<UpdateDamageModel>? updateDamageUIState;
@@ -22,67 +21,67 @@ class DriverLoadDetailsState extends Equatable {
     this.lpLoadById,
     this.uploadDamageUIState,
     this.damageListUIState,
-     this.loadStatusUIState,
+    this.loadStatusUIState,
     this.isUpdateDamage,
-     this.tripDocumentList,
-     this.deleteDamageUIState,
-     this.createDamageUIState,
-     this.settlementUIState,
-     this.updateDamageUIState,
-     this.locationDistance,
-     this.loadStatusId,
-     this.loadStatus = LoadStatus.assigned,
+    this.tripDocumentList,
+    this.deleteDamageUIState,
+    this.createDamageUIState,
+    this.settlementUIState,
+    this.updateDamageUIState,
+    this.locationDistance,
+    this.loadStatusId,
+    this.loadStatus = LoadStatus.assigned,
   });
 
   DriverLoadDetailsState copyWith({
-     UIState<TrackingDistanceResponse>? trackingDistance,
+    UIState<TrackingDistanceResponse>? trackingDistance,
     UIState<DriverLoadDetailsModel>? lpLoadById,
     UIState<UploadDamageFileModel>? uploadDamageUIState,
     UIState<GetDamageListModel>? damageListUIState,
     UIState<VpLoadAcceptModel>? loadStatusUiUpdate,
     UIState<DeleteDamageModel>? deleteDamageUIState,
     List<DocumentEntity>? tripDocumentList,
-     UIState<DamageModel>? createDamageUIState,
-     UIState<DamageModel>? settlementUIState,
-     UIState<UpdateDamageModel>? updateDamageUIState,
+    UIState<DamageModel>? createDamageUIState,
+    UIState<DamageModel>? settlementUIState,
+    UIState<UpdateDamageModel>? updateDamageUIState,
     bool? isUpdateDamage,
-     String? locationDistance,
-     int? loadStatusId,
-     LoadStatus? loadStatus,
+    String? locationDistance,
+    int? loadStatusId,
+    LoadStatus? loadStatus,
   }) {
     return DriverLoadDetailsState(
-       trackingDistance: trackingDistance ?? this.trackingDistance,
+      trackingDistance: trackingDistance ?? this.trackingDistance,
       lpLoadById: lpLoadById ?? this.lpLoadById,
       uploadDamageUIState: uploadDamageUIState ?? this.uploadDamageUIState,
       damageListUIState: damageListUIState ?? this.damageListUIState,
       isUpdateDamage: isUpdateDamage ?? this.isUpdateDamage,
       loadStatusUIState: loadStatusUiUpdate ?? this.loadStatusUIState,
-      tripDocumentList:tripDocumentList?? this.tripDocumentList ,
-       deleteDamageUIState: deleteDamageUIState ?? this.deleteDamageUIState,
-       createDamageUIState: createDamageUIState ?? this.createDamageUIState,
-       settlementUIState : settlementUIState ?? this.settlementUIState,
-       updateDamageUIState: updateDamageUIState ?? this.updateDamageUIState,
-       locationDistance: locationDistance ?? this.locationDistance,
-        loadStatusId: loadStatusId?? this.loadStatusId,
-        loadStatus: loadStatus ?? this.loadStatus, 
-     );
+      tripDocumentList: tripDocumentList ?? this.tripDocumentList,
+      deleteDamageUIState: deleteDamageUIState ?? this.deleteDamageUIState,
+      createDamageUIState: createDamageUIState ?? this.createDamageUIState,
+      settlementUIState: settlementUIState ?? this.settlementUIState,
+      updateDamageUIState: updateDamageUIState ?? this.updateDamageUIState,
+      locationDistance: locationDistance ?? this.locationDistance,
+      loadStatusId: loadStatusId ?? this.loadStatusId,
+      loadStatus: loadStatus ?? this.loadStatus,
+    );
   }
 
   @override
   List<Object?> get props => [
-        loadStatus,
-        lpLoadById,
-        uploadDamageUIState,
-        damageListUIState,
-        isUpdateDamage,
-        loadStatusUIState,
-        tripDocumentList,
-        deleteDamageUIState,
-        createDamageUIState,
-        settlementUIState,
-        updateDamageUIState,
-         trackingDistance,
-          locationDistance,
-         loadStatusId,  
-      ];
+    loadStatus,
+    lpLoadById,
+    uploadDamageUIState,
+    damageListUIState,
+    isUpdateDamage,
+    loadStatusUIState,
+    tripDocumentList,
+    deleteDamageUIState,
+    createDamageUIState,
+    settlementUIState,
+    updateDamageUIState,
+    trackingDistance,
+    locationDistance,
+    loadStatusId,
+  ];
 }
