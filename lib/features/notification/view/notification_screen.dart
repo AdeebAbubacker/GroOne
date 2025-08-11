712201e2207b24e02db337838504afb7924ed52e
ishwar parihar <72557736+ishwar-parihar2610@users.noreply.github.com>
2025-08-11 15:54:40 +0530

    started notification work

---
+++ lib/features/notification/view/notification_screen.dart
@@ -0,0 +1,28 @@
+// import 'package:flutter/material.dart';
+// import 'package:gro_one_app/utils/app_application_bar.dart';
+// import 'package:gro_one_app/utils/app_colors.dart';
+//
+// class NotificationScreen extends StatefulWidget {
+//   const NotificationScreen({super.key});
+//
+//   @override
+//   State<NotificationScreen> createState() => _NotificationScreenState();
+// }
+//
+// class _NotificationScreenState extends State<NotificationScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: AppColors.white,
+//       appBar: CommonAppBar(
+//         backgroundColor: AppColors.white,
+//         title: context.appText.tripScheduling,
+//       ),
+//       body: _notificationView()
+//     );
+//   }
+//
+//   Widget _buildNotification(){
+//
+//   }
+// }
